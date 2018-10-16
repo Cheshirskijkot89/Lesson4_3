205e17871fdcdfe8013f1454b2d52b5f16dfe931
Andrij Chizhenko <chizh.and.ol@gmail.com>
2018-10-16 12:11:35 +0300

    1st commit

---
+++ src/chizhenko/Main.java
@@ -0,0 +1,13 @@
+package chizhenko;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		for (int i=1;i<=9;i++) {
+			System.out.println("" + i + " * 5 = " +  i*5);
+		}
+
+	}
+
+}
